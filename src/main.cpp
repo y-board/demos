ae69d62ee3b600fa606a28dd39f0cccbd826122c
Philip Lundrigan <lundrigan@byu.edu>
2023-07-31 09:32:03 -0600

    Add demo to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,12 @@
+#include "hardware_test/hardware_test.h"
 #include "wifi_sniffer/wifi_sniffer.h"
 
-void setup() { wifi_sniffer_init(); }
+void setup() {
+    // wifi_sniffer_init();
+    hardware_test_init();
+}
 
-void loop() { wifi_sniffer_loop(); }
+void loop() {
+    // wifi_sniffer_loop();
+    hardware_test_loop();
+}
