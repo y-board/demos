cc58a7b4e68f33d22cde996de910ae2856fe0a45
Philip Lundrigan <lundrigan@byu.edu>
2024-05-31 20:10:50 -0600

    Improve design of game

--- y-board-v3/light_dial_game/src/main.cpp
+++ y-board-v3/light_dial_game/src/main.cpp
@@ -2,13 +2,11 @@
 #include "Arduino.h"
 #include "yboard.h"
 
-#define CODE_LENGTH 3
-
 typedef struct {
     unsigned char r, g, b;
 } Color;
 
-void generate_secret_code();
+int generate_random_number();
 void play_correct_guess();
 void play_bad_guess();
 void play_win();
@@ -16,7 +14,8 @@ int filter(int value);
 Color map_to_color(int value, int min_value, int max_value);
 void delay_with_audio(int ms);
 
-int secret_code[CODE_LENGTH];
+const int code_length = 3;
+int secret_number;
 int success_count = 0;
 
 void setup() {
@@ -26,7 +25,7 @@ void setup() {
     Yboard.set_all_leds_color(0, 0, 0);
     Yboard.set_led_brightness(100);
 
-    generate_secret_code();
+    secret_number = generate_random_number();
 }
 
 void loop() {
@@ -34,23 +33,23 @@ void loop() {
 
     for (int i = 1; i <= Yboard.led_count; i++) {
         if (i == led_value) {
-            Color c = map_to_color(abs(secret_code[success_count] - i), 0, 10);
+            Color c = map_to_color(abs(secret_number - i), 0, 10);
             Yboard.set_led_color(i, c.r, c.g, c.b);
         } else {
             Yboard.set_led_color(i, 0, 0, 0);
         }
     }
 
-    if (Yboard.get_button(1) && led_value == secret_code[success_count]) {
+    if (Yboard.get_button(1) && led_value == secret_number) {
         success_count++;
         play_correct_guess();
+        secret_number = generate_random_number();
 
-        if (success_count == CODE_LENGTH) {
+        if (success_count == code_length) {
             play_win();
 
             // Reset the game
             success_count = 0;
-            generate_secret_code();
         }
 
         while (Yboard.get_button(1))
@@ -62,15 +61,19 @@ void loop() {
     }
 }
 
-void generate_secret_code() {
-    for (int i = 0; i < CODE_LENGTH; i++) {
-        secret_code[i] = random(1, 21);
-    }
+int generate_random_number() {
+    int new_rand;
+
+    do {
+        new_rand = random(1, Yboard.led_count + 1);
+    } while (new_rand == secret_number);
+
+    return new_rand;
 }
 
 void play_correct_guess() {
     char file_name[50];
-    snprintf(file_name, 50, "/light_game/sm64_red_coin_%d.wav", success_count % (CODE_LENGTH + 1));
+    snprintf(file_name, 50, "/light_game/sm64_red_coin_%d.wav", success_count % (code_length + 1));
     Serial.println(file_name);
 
     Yboard.play_song_from_sd(file_name);
@@ -89,17 +92,16 @@ void play_bad_guess() {
 
 void play_win() {
     Yboard.play_song_from_sd("/light_game/sm64_key_get.wav");
-    for (int i = 0; i < CODE_LENGTH; i++) {
-        Yboard.set_led_color(secret_code[i], 0, 255, 0);
-        delay_with_audio(250);
-        Yboard.set_led_color(secret_code[i], 0, 0, 0);
-        delay_with_audio(250);
-    }
 
     for (int i = 1; i <= Yboard.led_count; i++) {
         Yboard.set_led_color(i, 0, 255, 0);
-        delay_with_audio(80);
+        delay_with_audio(100);
     }
+
+    Yboard.set_all_leds_color(0, 0, 0);
+    delay_with_audio(250);
+    Yboard.set_all_leds_color(0, 255, 0);
+
     delay_with_audio(500);
     Yboard.set_all_leds_color(0, 0, 0);
 }
