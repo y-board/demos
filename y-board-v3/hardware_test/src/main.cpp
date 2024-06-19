37652dfbbe2dc5103806f20fab6cbc0946735134
Philip Lundrigan <lundrigan@byu.edu>
2024-06-18 21:15:28 -0600

    Improve hardware test
    - Test all LEDs
    - Play notes and a song
    - Print out accelerometer and temperature data

--- y-board-v3/hardware_test/src/main.cpp
+++ y-board-v3/hardware_test/src/main.cpp
@@ -5,7 +5,6 @@
 void setup() {
     Serial.begin(9600);
     Yboard.setup();
-    Yboard.play_song_from_sd("song.mp3");
 }
 
 void loop() {
@@ -22,14 +21,17 @@ void loop() {
     }
 
     if (Yboard.get_button(1)) {
-        Yboard.set_led_color(3, 255, 0, 0);
-        Yboard.loop_speaker();
+        Yboard.set_all_leds_color(255, 255, 255);
+        Yboard.play_notes("O5 T150 AA#B");
+        Yboard.set_all_leds_color(0, 0, 0);
     } else {
         Yboard.set_led_color(3, 0, 0, 0);
     }
 
     if (Yboard.get_button(2)) {
-        Yboard.set_led_color(4, 255, 0, 0);
+        Yboard.set_all_leds_color(255, 255, 255);
+        Yboard.play_sound_file("/hardware_test/sm64_key_get.wav");
+        Yboard.set_all_leds_color(0, 0, 0);
     } else {
         Yboard.set_led_color(4, 0, 0, 0);
     }
@@ -38,7 +40,7 @@ void loop() {
 
     if (Yboard.accelerometer_available()) {
         accelerometer_data accel_data = Yboard.get_accelerometer();
-        // Serial.printf("x: %f, y: %f, z: %f\n", accel_data.x, accel_data.y, accel_data.z);
+        Serial.printf("x: %f, y: %f, z: %f\n", accel_data.x, accel_data.y, accel_data.z);
         Yboard.set_led_color(6, map(accel_data.x, -1000, 1000, 0, 255), 0, 0);
         Yboard.set_led_color(7, map(accel_data.y, -1000, 1000, 0, 255), 0, 0);
         Yboard.set_led_color(8, map(accel_data.z, -1000, 1000, 0, 255), 0, 0);
@@ -56,5 +58,5 @@ void loop() {
     } else {
         Yboard.set_led_color(10, 0, 0, 0);
     }
-    // Serial.printf("temp: %f, humidity: %f\n", temp_data.temperature, temp_data.humidity);
+    Serial.printf("temp: %f, humidity: %f\n", temp_data.temperature, temp_data.humidity);
 }
