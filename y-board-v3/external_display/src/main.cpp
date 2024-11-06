d529f59b4c0a5c25cefce1c56283b0f19fbad6a5
Philip Lundrigan <lundrigan@byu.edu>
2024-11-06 08:39:43 -0700

    Update display demo for new library

--- y-board-v3/external_display/src/main.cpp
+++ y-board-v3/external_display/src/main.cpp
@@ -1,36 +1,11 @@
-
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-#include <Arduino.h>
 #include <yboard.h>
 
-Adafruit_SSD1306 display(128, 64); // Create display
-
 int old_knob_value = 0;
-int char_count = 0;
 
 void setup() {
     Serial.begin(115200);
     Yboard.setup();
-
-    Serial.println("Drawing test on display");
-    if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3c)) {
-        Serial.println("Error initializing display");
-        return;
-    }
-    display.clearDisplay();
-    display.setTextColor(WHITE);
-    display.setRotation(90);
-    display.setTextWrap(true);
-
-    display.setTextSize(1);
-    display.clearDisplay();
-    display.setCursor(20, 40);
-
-    display.drawRoundRect(0, 0, 128, 64, 5, WHITE);
-    display.display();
-
-    Serial.println("Done drawing test on display");
+    Yboard.display.setTextSize(2);
 }
 
 int filter(int value) {
@@ -59,27 +34,19 @@ int get_knob_value() {
 }
 
 void loop() {
-    int knob_value = get_knob_value();
 
-    // Remove oscillation of knob value by filtering
+    int knob_value = get_knob_value();
 
     if (knob_value != old_knob_value) {
         // Only update the display if something has changed
-        display.drawChar(char_count * 8 + 4, 4, (char)knob_value, WHITE, BLACK, 1);
-        display.display();
+        Yboard.display.clearDisplay();
+        Yboard.display.setCursor(0, 10);
+        Yboard.display.printf("Knob: %d\n", knob_value);
+        Yboard.display.display();
 
         Serial.printf("Knob value: %d\n", knob_value);
 
         old_knob_value = knob_value;
-    }
-
-    if (Yboard.get_button(1)) {
-        char_count++;
-
-        while (Yboard.get_button(1)) {
-            // Wait for button to be released
-            delay(10);
-        }
     }
 
     delay(50);
