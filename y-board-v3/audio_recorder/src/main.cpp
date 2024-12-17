4ceb314b2f45485929b8b6e3c5b934f88248a78c
Philip Lundrigan <lundrigan@byu.edu>
2024-12-17 10:37:27 -0700

    Add recording animation

--- y-board-v3/audio_recorder/src/main.cpp
+++ y-board-v3/audio_recorder/src/main.cpp
@@ -10,11 +10,19 @@ static const float smoothness_beta = 0.5;   // shifts the gaussian to be symmetr
 unsigned long previousMillis = 0; // Stores last time the action was performed
 unsigned int smoothness_index = 0;
 
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 32
+#define NUM_BARS 16
+#define BAR_WIDTH 6
+#define MAX_BAR_HEIGHT (SCREEN_HEIGHT - 5)
+
+void draw_animation();
+
 void setup() {
     Serial.begin(9600);
     Yboard.setup();
     Yboard.set_sound_file_volume(10);
-    Yboard.set_recording_volume(3);
+    Yboard.set_recording_volume(8);
     Yboard.set_all_leds_color(255, 255, 255);
 }
 
@@ -26,6 +34,7 @@ void loop() {
         while (Yboard.get_button(1)) {
             if (started_recording) {
                 Yboard.set_all_leds_color(255, 0, 0);
+                draw_animation();
                 delay(100);
             } else {
                 Yboard.set_all_leds_color(255, 0, 0);
@@ -40,6 +49,8 @@ void loop() {
             Yboard.stop_recording();
         }
 
+        Yboard.display.clearDisplay();
+        Yboard.display.display();
         Yboard.set_all_leds_color(0, 0, 0);
     }
 
@@ -72,4 +83,25 @@ void loop() {
             smoothness_index = 0;
         }
     }
+}
+
+void draw_animation() {
+    Yboard.display.clearDisplay();
+
+    Yboard.display.setTextSize(1);
+    Yboard.display.fillCircle(3, 3, 2, WHITE);
+    Yboard.display.drawChar(8, 0, 'R', WHITE, BLACK, 1);
+    Yboard.display.drawChar(16, 0, 'E', WHITE, BLACK, 1);
+    Yboard.display.drawChar(24, 0, 'C', WHITE, BLACK, 1);
+
+    // Draw equalizer bars with random heights to simulate sound levels
+    for (int i = 0; i < NUM_BARS; i++) {
+        int barHeight = random(5, MAX_BAR_HEIGHT); // Random height between 5 and max height
+        int x = i * (BAR_WIDTH + 2);               // Calculate x position of the bar
+
+        // Draw the bar (bottom-up)
+        Yboard.display.fillRect(x, SCREEN_HEIGHT - barHeight, BAR_WIDTH, barHeight, WHITE);
+    }
+
+    Yboard.display.display();
 }
