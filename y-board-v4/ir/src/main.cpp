41524c543081c6210c88e82607a3e8a5097c6f8a
Philip Lundrigan <lundrigan@byu.edu>
2025-05-29 20:30:47 -0600

    Make the demo better

--- y-board-v4/ir/src/main.cpp
+++ y-board-v4/ir/src/main.cpp
@@ -1,23 +1,30 @@
 #include "yboard.h"
 
+int brightness = 120;
+
+void print_to_display(const char *message) {
+    Yboard.display.clearDisplay();
+    Yboard.display.setCursor(0, 0);
+    Yboard.display.println(message);
+    Yboard.display.display();
+}
+
 void setup() {
     Serial.begin(115200);
     delay(1000); // Wait for Serial to initialize
 
     Yboard.setup();
+    Yboard.set_led_brightness(brightness);
 }
 
 void loop() {
     if (Yboard.get_switch(1)) {
-        Yboard.set_all_leds_color(0, 255, 0);
+        print_to_display("IR Receiver Mode");
 
         if (Yboard.recv_ir()) {
             decode_results &results = Yboard.ir_results;
             Serial.println(resultToHumanReadableBasic(&results));
-            Yboard.display.clearDisplay();
-            Yboard.display.setCursor(0, 0);
-            Yboard.display.println(resultToHumanReadableBasic(&results));
-            Yboard.display.display();
+            print_to_display(resultToHumanReadableBasic(&results).c_str());
 
             // My own custom protocol
             if (results.value == 0x01) {
@@ -38,11 +45,27 @@ void loop() {
                 Yboard.set_all_leds_color(0, 255, 0);
             } else if (results.value == 0xFF50AF) {
                 Yboard.set_all_leds_color(0, 0, 255);
+            } else if (results.value == 0xFF609F) {
+                Yboard.set_all_leds_color(0, 0, 0);
+            } else if (results.value == 0xFFE01F) {
+                Yboard.set_all_leds_color(255, 255, 255);
+            } else if (results.value == 0xFFA05F) {
+                // Add brightness
+                brightness += 10;
+                if (brightness > 255) {
+                    brightness = 255;
+                }
+                Yboard.set_led_brightness(brightness);
+            } else if (results.value == 0xFF20DF) {
+                // Subtract brightness
+                brightness -= 10;
+                if (brightness < 0) {
+                    brightness = 0;
+                }
+                Yboard.set_led_brightness(brightness);
             }
 
             delay(1000);
-            Yboard.set_all_leds_color(0, 255, 0);
-
             Yboard.clear_ir();
         }
     } else {
